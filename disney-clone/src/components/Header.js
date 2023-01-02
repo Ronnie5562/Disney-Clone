5540634e45cdadb0b0020d7e60b1a2fe88f3be8e
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-02 22:01:30 +0100

    A new commit

--- disney-clone/src/components/Header.js
+++ disney-clone/src/components/Header.js
@@ -8,10 +8,31 @@ const Header = (props) =>{
             </Logo>
             <NavMenu>
                 <a href="/home">
-                    <img src="./images/home-icon.svg" />
+                    <img src="./images/home-icon.svg" alt="HomeIcon" />
                     <span>HOME</span>
                 </a>
+                <a href="">
+                    <img src="./images/search-icon.svg" alt="SearchIcon" />
+                    <span>SEARCH</span>
+                </a>
+                <a href="/watchlist">
+                    <img src="./images/watchlist-icon.svg" alt="watchlistIcon" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href="/originals">
+                    <img src="./images/original-icon.svg" alt="originalsIcon" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href="/movies">
+                    <img src="./images/movie-icon.svg" alt="moviesIcon" />
+                    <span>MOVIES</span>
+                </a>
+                <a href="/series">
+                    <img src="./images/series-icon.svg" alt="seriesIcon" />
+                    <span>SERIES</span>
+                </a>
             </NavMenu>
+            <Login>LOG IN</Login>
         </Nav>
     )
 };
@@ -76,39 +97,58 @@ const NavMenu = styled.div`
             padding: 2px 0px;
             white-space: nowrap;
             position: relative;
+
+
+            // The real cool stuff!!! you would probaly need this later;
+            
+            &:before {
+                background-color: rgb(249, 249, 249);
+                border-radius: 0px 0px 4px 4px;
+                bottom: -6px;
+                content: "";
+                height: 2px;
+                left: 0px;
+                opacity: 0;
+                position: absolute;
+                right: 0px;
+                transform-origin: left center;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+                visibility: hidden;
+                width: auto;
+            }
         }
         
-        // The real cool stuff!!! you would probaly need this later;
-        
-        &::before {
-            background-color: rgb(249, 249, 249);
-            border-radius: 0px 0px 4px 4px;
-            bottom: -6px;
-            content: "";
-            height: 2px;
-            opacity: 0;
-            position: absolute;
-            right: 0px;
-            transform-origin: left center;
-            transform: scaleX(0);
-            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
-            visibility: hidden;
-            width: auto;
+        &:hover{
+            span:before{
+                transform: scaleX(1);
+                visibility: visible;
+                opacity: 1 !important;
+            }
         }
     }
 
-    &:hover{
-        span:before{
-            transform: skewX(1);
-            visibility: visible;
-            opacity: 1 !important;
-        }
-    }
 
     /* @media screen and (max-width: 768px) {
         display: none;
     } */
 `;
+
+const Login = styled.a`
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all 0.2s ease 0s;
+
+    &:hover{
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
+    }
+`; 
 
 
 export default Header;
